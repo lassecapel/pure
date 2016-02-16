7cd2fe3e49604cf95b18efbe98bc19c35bb758b5
LC <l.capel@youngcapital.nl>
2016-02-16 08:16:45 +0100

    minor

--- source/store/constants/action_types.js
+++ source/store/constants/action_types.js
@@ -1,3 +1,4 @@
 export const ADD_SLIDE = 'ADD_SLIDE';
 export const DELETE_SLIDE = 'DELETE_SLIDE';
 export const EDIT_SLIDE = 'EDIT_SLIDE';
+export const SET_INDEX = 'SET_INDEX';
--- source/store/reducers/slides/index.js
+++ source/store/reducers/slides/index.js
@@ -1,18 +1,16 @@
-import { ADD_SLIDE, DELETE_SLIDE, EDIT_SLIDE } from '../../constants/action_types';
+import { ADD_SLIDE, DELETE_SLIDE, EDIT_SLIDE, SET_INDEX } from '../../constants/action_types';
+import { combineReducers } from 'redux';
+
 const assign = Object.assign;
-const initialState = {
-  activeIndex: 0,
-  slides: [
+const initialSlides = [
     {
       id: 1,
       title: 'Change me..',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sollicitudin dictum enim sed convallis. Pellentesque viverra justo sit amet nibh malesuada, dictum mattis orci mollis. Nunc ut lectus in ex lobortis suscipit.'
     }
-  ],
-};
+  ];
 
-export default (
-  state = initialState, {type, id, title, text, index} = {}) => {
+const slides = (state = initialSlides, {type, id, title, text} = {}) => {
   switch (type) {
     case ADD_SLIDE:
       return [
@@ -37,16 +35,26 @@ export default (
             text: text }) :
           slide
       );
-
-    case SET_INDEX:
-      return Object.assign({},
-        state,
-        {
-          activeIndex: index,
-        }
-      )
-
     default:
       return state;
   }
 };
+
+function activeId (state = {activeIndex: 0}, {type, index } = {}) {
+  switch (type) {
+    case SET_INDEX:
+      return assign({},
+        ...state,
+        {
+          activeIndex: index
+        }
+      );
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  slides,
+  activeId
+});
